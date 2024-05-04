269b6041b54f91f4c16be5451bd261e7f6a320de
ismailxkhalid <ismailxkhalid@gmail.com>
2024-05-04 12:42:00 +0300

    add jwt verification middleware

---
+++ src/middleware/authenticate.ts
@@ -0,0 +1,33 @@
+import Jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import createHttpError from 'http-errors';
+import { config } from '../config/config';
+
+export interface AuthRequest extends Request {
+    userId: string;
+}
+
+const authenticate = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const token = req.header('Authorization');
+    if (!token) {
+        res.status(401).json({ error: 'Authorization token required' });
+        return next(createHttpError(401, 'Authorization token required'));
+    }
+
+    try {
+        const parsedToken = token.split(' ')[1];
+        const decoded = Jwt.verify(parsedToken, config.secret as string);
+
+        const _req = req as AuthRequest;
+        _req.userId = decoded.sub as string;
+        next();
+    } catch (error) {
+        return next(createHttpError(401, error as string));
+    }
+};
+
+export default authenticate;
--- src/routes/bookRouter.ts
+++ src/routes/bookRouter.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import path, { resolve } from 'node:path';
 import { allBooks, createBook } from '../controllers/bookController';
+import authenticate from '../middleware/authenticate';
 const router = express.Router();
 
 const upload = multer({
@@ -13,6 +14,7 @@ router.get('/', allBooks);
 //Login Route
 router.post(
     '/',
+    authenticate,
     upload.fields([
         { name: 'coverImage', maxCount: 1 },
         { name: 'file', maxCount: 1 }
